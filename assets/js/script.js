a69c3d130b3e590ef1e2ac3a21949dde3b1e88e9
Zachary Clark <zacclark369@gmail.com>
2021-12-15 16:55:56 -0800

    changed styling of buttons, organized columns, divs to be filled by api ready

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,14 @@
 // initialize dropdown menu
 var dropdowns = document.querySelectorAll('.dropdown-trigger')
 for (var i = 0; i < dropdowns.length; i++){
-    M.Dropdown.init(dropdowns[i]);
+    M.Dropdown.init(dropdowns[i], "right");
 }
+
+// document.addEventListener('DOMContentLoaded', function() {
+//     var dropdown1 = document.querySelector('.simple-dropdown');
+//     var dropdownOptions = {
+//         'closeOnClick': true,
+//         'hover':true
+//     }
+//     var instanceDropdown1 = M.Dropdown.init(dropdown1, dropdownOptions);
+// });
