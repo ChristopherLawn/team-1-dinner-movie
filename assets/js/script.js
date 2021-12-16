a90dec6a389c711405046077ce8a771150acd947
DrDano <danielharned@gmail.com>
2021-12-15 20:32:46 -0800

    adding recent changes to index.html and style.css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,20 @@
 // get OMDB movie database data and movie poster
 // give user a search bar to search for movie titles
-// return movie poster along with synopsis and release year
+// return movie poster along with synopsis and release year for EVERY result that api returns
+// If user clicks on one of these titles, will call api again and get more detailed results
 var getMovieData = function (movieTitle) {
     var apiKey = ""
     
     fetch(`http://www.omdbapi.com/?apikey=${apiKey}&s=${movieTitle}&type=movie&r=json`).then(function (response) {
         if (response.ok) {
             response.json().then(function(data) {
-                console.log(data)
+                var dataArr = data.Search
+                for (var i = 0; i < dataArr.length; i++) {
+                    var imdbID = dataArr[i].imdbID;
+                    var Title = dataArr[i].Title;
+                    var Year = dataArr[i].Year;
+                    var Poster = dataArr[i].Poster;
+                }
             })
         } else {
             console.log("response is not ok")
