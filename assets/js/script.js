694e6032e110771c44757c763642babbd7f5521a
Zachary Clark <zacclark369@gmail.com>
2021-12-16 21:02:35 -0800

    created new js file and added example carousel html w/food images

--- assets/js/script.js
+++ assets/js/script.js
@@ -75,19 +75,3 @@ var displayRestaurants = function (data) {
   }
 };
 
-
-// initialize dropdown menu
-var dropdowns = document.querySelectorAll('.dropdown-trigger')
-for (var i = 0; i < dropdowns.length; i++){
-    M.Dropdown.init(dropdowns[i]);
-}
-
-// document.addEventListener('DOMContentLoaded', function() {
-//     var dropdown1 = document.querySelector('.simple-dropdown');
-//     var dropdownOptions = {
-//         'closeOnClick': true,
-//         'hover':true
-//     }
-//     var instanceDropdown1 = M.Dropdown.init(dropdown1, dropdownOptions);
-// });
-
